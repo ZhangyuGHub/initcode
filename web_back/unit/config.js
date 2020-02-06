87eb12ebe04b1e5cffdfdf9ac7c73fa8eee2e1f9
ZhangyuGHub <1394956959@qq.com>
2020-02-06 21:35:35 +0800

    修改了管理员推出的地址

--- web_back/unit/config.js
+++ web_back/unit/config.js
@@ -1,6 +1,6 @@
 var BASURL = "http://192.168.43.101:8000";
 var API = {
-    logout: BASURL + "/admin/loginout",
+    logout: BASURL + "/admin/logout",
     login: BASURL + "/admin/login",
     loginfo: BASURL + "/admin/getuser"
 }
