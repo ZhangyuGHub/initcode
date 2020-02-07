3f256cf9a09a862a8421f75fc379342f2f94638b
ZhangyuGHub <1394956959@qq.com>
2020-02-07 15:15:04 +0800

    新增文章类型的获取，增加，删除

---
+++ web_back/unit/category.js
@@ -0,0 +1,20 @@
+// 与文章相关的操作
+// 获取文章类别
+var category_get = {
+    get: function () {
+        return $.get(API.category_get)
+
+    }
+}
+// 添加文章类别
+var category_add = {
+    add: function (name, slug) {
+        return $.post(API.category_add, { "name": name, "slug": slug })
+    }
+}
+// 删除文章类别
+var category_del = {
+    del: function (id) {
+        return $.post(API.category_del, { "id": id });
+    }
+}
--- web_back/unit/config.js
+++ web_back/unit/config.js
@@ -1,6 +1,16 @@
-var BASURL = "http://192.168.43.101:8000";
+var BASURL = "http://192.168.31.101:8000";
 var API = {
+    //退出地址
     logout: BASURL + "/admin/logout",
+    // 登录地址
     login: BASURL + "/admin/login",
-    loginfo: BASURL + "/admin/getuser"
+    // 获取用户信息
+    loginfo: BASURL + "/admin/getuser",
+    // 获取文章类别
+    category_get: BASURL + "/admin/category_search",
+    // 增加文章类别
+    category_add: BASURL + "/admin/category_add",
+    // 删除文章类别
+    category_del: BASURL + "/admin/category_delete",
+
 }
